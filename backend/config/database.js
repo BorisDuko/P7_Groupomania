99068a2026e0ac2f7de9f086a24da7eb52faddbc
BorisDuko <boris.duko@greta-cfa-aquitaine.academy>
2022-12-06 16:27:37 +0100

    post controllers refactored

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-});
-// .promise()
-// to use async/await
+const connection = mysql
+  .createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  })
+  .promise();
 
 connection.connect((error) => {
   if (error) throw error;
-  // console.log("Successfully connected to db_p7_duko");
+  console.log("Successfully connected to db_p7_duko");
 });
 
 module.exports = connection;
--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,12 +1,14 @@
 const connection = require("../config/database");
 
 // ALL POSTS
-exports.getAllPosts = (req, res, next) => {
-  connection.query("SELECT * FROM post_table", (err, results) => {
-    if (err) throw err;
-    // console.log(results);
-    res.status(200).json(results);
-  });
+exports.getAllPosts = async (req, res, next) => {
+  try {
+    const [rows] = await connection.query("SELECT * FROM user_table");
+    res.status(200).send(rows);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 };
 //--------------------------------------
 // -- -- Get all posts with a mark for those read by a specific user --
@@ -17,42 +19,42 @@ LEFT JOIN read_table r ON p.p_id = r.r_post_id
 ORDER BY p.p_date_published DESC`;
 //--------------------------------------
 // GET ONE POST
-exports.getOnePost = (req, res, next) => {
+exports.getOnePost = async (req, res, next) => {
   let post_id = req.params.id;
-  let sql = "SELECT * FROM post_table WHERE p_id=?";
-  connection.query(
-    sql,
-    post_id,
-
-    (err, results) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(results);
-      res.status(200).json(results);
-    }
-  );
+  try {
+    const [rows] = await connection.query(
+      `
+    SELECT *
+    FROM post_table
+    WHERE p_id=?
+    `,
+      [post_id]
+    );
+    res.status(200).send(rows);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 };
 // CREATE POST
-exports.createOnePost = (req, res, next) => {
+exports.createOnePost = async (req, res, next) => {
   let front_p_author_id = req.body.p_author_id;
   let front_p_text = req.body.p_text;
   let front_p_image_url = req.body.p_image_url;
-
-  connection.query(
-    `INSERT INTO 
-    post_table (p_author_id, p_text, p_image_url) 
-    VALUES (?, ?, ?)`,
-    [front_p_author_id, front_p_text, front_p_image_url],
-    (err, results) => {
-      // if (err) throw err;
-      if (err) {
-        console.log(err);
-      }
-      console.log("Post created successfully!");
-      res.status(201).json(results);
-    }
-  );
+  try {
+    const result = await connection.query(
+      `
+    INSERT INTO
+    post_table (p_author_id, p_text, p_image_url)
+    VALUES (?, ?, ?)
+    `,
+      [front_p_author_id, front_p_text, front_p_image_url]
+    );
+    res.status(201).send("Post created");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 };
 
 // ------------------------------
